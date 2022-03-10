d10c1c5832e09c53781e0119b99a176456744c92
huang <1300913599@qq.com>
2022-03-10 18:43:28 +0800

    部署准备

--- sote-provider-user-8001/src/main/java/com/upc/eden/user/UserApplication.java
+++ sote-provider-user-8001/src/main/java/com/upc/eden/user/UserApplication.java
@@ -3,13 +3,12 @@ package com.upc.eden.user;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @MapperScan(value = "com.upc.eden.user.mapper")
-@SpringBootApplication
 public class UserApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
     }
-
 }
