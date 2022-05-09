e84ccf4c996adc31c5f76a6afa1d3e0d2cb3b5ec
huang <1300913599@qq.com>
2022-05-09 11:25:16 +0800

    socket

---
+++ sote-auth-9401/src/main/java/com/upc/eden/auth/config/WebSocketConfig.java
@@ -0,0 +1,18 @@
+package com.upc.eden.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+
+/**
+ * @author hdh
+ * @date 2022/5/9 - 11:13
+ */
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter(){
+        return new ServerEndpointExporter();
+    }
+}
